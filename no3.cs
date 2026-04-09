e6f8ddd994ace5f1b5023e4f29fd45879f62d925
Samara-Zaman <samarazaman55@gmail.com>
2026-04-09 13:46:08 +0600

    Create no3.cs

---
+++ no3.cs
@@ -0,0 +1,15 @@
+//Take an integer from user input and print it. 
+
+using System;
+
+class program{
+    static void Main(string[] args){
+        Console.WriteLine("Enter an integer value: ");
+        int i = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Your have entered:{i}");
+        
+    }
+    
+    
+    
+}
