e8ec401d6580934a4d1c1086ee6037ef00238cde
Samara-Zaman <samarazaman55@gmail.com>
2026-04-09 08:16:53 +0600

    Create no1.cs

---
+++ no1.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program// everything should be under a class in c#, even the main function
+{
+     static void Main(string[] args)
+    {
+    
+        Console.WriteLine("Hello World!");
+    }
+
+}
