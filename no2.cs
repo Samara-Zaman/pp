a9c01949b66ebde8aa1b04d1aca0537b858ee1bf
Samara-Zaman <samarazaman55@gmail.com>
2026-04-09 13:19:05 +0600

    Create no2.cs

---
+++ no2.cs
@@ -0,0 +1,21 @@
+//Declare variables of type int, double, char, bool, and print them. 
+using System;
+
+class program{
+    
+    static void Main(string[] args){
+        
+        int i = 10;
+        double d = 10.123;
+        char c = 'A';// use 'single quotation' for character
+        bool b = true;
+        
+        Console.WriteLine($"integer:{i},double:{d},char:{c},bool:{b}");
+        
+        
+        
+    }
+        
+    
+    
+}
